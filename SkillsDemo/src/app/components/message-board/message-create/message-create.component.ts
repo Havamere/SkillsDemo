da8bdc85dc75eddb0962b78e00b7e16f136c7770
Kyle R <kyle.rinehart83@gmail.com>
2021-02-26 17:15:59 -0500

    Fixed default text issue by placing label beside selector, changed selector to logic based build, replaced dark option with light option on selector and moved colors around for better RGB look

--- SkillsDemo/src/app/components/message-board/message-create/message-create.component.ts
+++ SkillsDemo/src/app/components/message-board/message-create/message-create.component.ts
@@ -6,9 +6,63 @@ import { Component, EventEmitter, Output } from '@angular/core';
 	styleUrls: ['./message-create.component.sass']
 })
 export class MessageCreateComponent {
-	selectedColor ='';
+	selectedColor = '';
 	enteredTitle = '';
 	enteredContent = '';
+
+	messageColorArray = [
+
+		{
+			name: 'White', 
+			value: 'is-white', 
+			style: 'background-color: hsl(0, 0%, 100%)', 
+			font_color: 'hsl(0, 0%, 4%)'
+		},
+		{
+			name: 'Dark Blue', 
+			value: 'is-link', 
+			style: 'background-color: hsl(217, 71%, 53%)', 
+			font_color: '#fff'
+		},
+		{
+			name: 'Light Blue', 
+			value: 'is-info', 
+			style: 'background-color: hsl(204, 86%, 53%)', 
+			font_color: '#fff'
+		},
+		{
+			name: 'Aqua', 
+			value: 'is-primary', 
+			style: 'background-color: hsl(171, 100%, 41%)', 
+			font_color: '#fff'
+		},
+		{
+			name: 'Green', 
+			value: 'is-success', 
+			style: 'background-color: hsl(141, 53%, 53%)', 
+			font_color: '#fff'
+		},
+		{
+			name: 'Orange', 
+			value: 'is-warning', 
+			style: 'background-color: hsl(48, 100%, 67%)', 
+			font_color: 'rgba(0, 0, 0, 0.7)'
+		},
+		{
+			name: 'Red', 
+			value: 'is-danger', 
+			style: 'background-color: hsl(348, 100%, 61%)', 
+			font_color: '#fff'
+		},
+		{
+			name: 'Light', 
+			value: 'is-light', 
+			style: 'background-color: hsl(0, 0%, 96%)', 
+			font_color: 'hsl(0, 0%, 96%)'
+		}
+
+	];
+
 	@Output() messageCreated = new EventEmitter();
 
 	onAddMessage() {
